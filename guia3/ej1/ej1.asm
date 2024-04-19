9214c42f3014b158cbf898d50274c62a5fb0bdfe
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-04-19 10:49:15 -0300

    ajvp putoo

---
+++ guia3/ej1/ej1.asm
@@ -0,0 +1,21 @@
+;main.asm
+GLOBAL main
+EXTERN printf
+
+section .rodata
+fmt db “Cantidad de argumentos: %d\n”, 0
+
+section .text
+main:
+    push ebp ;Armado de stack frame 
+    mov ebp, esp ;
+
+    push dword [ebp+8]
+    push fmt
+    call printf
+    add esp, 2*4
+    mov eax, 0
+
+    mov esp, ebp
+    pop ebp
+    ret
