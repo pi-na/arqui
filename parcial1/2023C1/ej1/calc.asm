611fd255e7044d18c6ac2b3faa1e22f38453bbce
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-04-27 16:24:36 -0300

    nico arias

--- parcial1/2023C1/ej1/calc.asm
+++ parcial1/2023C1/ej1/calc.asm
@@ -4,13 +4,12 @@ extern puts
 extern operar
 
 global main
-ALIGN 4
 
 section .rodata:
-muls dd "*"
-sums dd "+"
-divs dd "/"
-pows dd "^" 
+muls db '*', 0
+sums db '+', 0
+divs db '/', 0
+pows db '^', 0
 ok_msg dd "Resultado = %d\n", 0
 error_msg dd "Error: operador <%s> no reconocido\n", 0
 
@@ -23,8 +22,10 @@ main:
     push ebp
     mov ebp, esp
     
+    pushad
+
     mov ebx, [ebp + 12]     ;   Cargo el puntero a argv (que es un array de punteros)   
-    mov ebx, [ebx]          ;   Desreferencio el puntero, entro al array argv   
+    ;mov ebx, [ebx]         ;   !! ESTA DE MAS !!
     add ebx, 4              ;   Salto a la segunda posicion de argv (puntero al primer argumento, char*)
 
     mov eax, [ebx]          ;   Cargo el primer operando
@@ -34,7 +35,7 @@ main:
         add ebx, 4              ;   Salto a la tercera posicion de argv (puntero al segundo argumento, operador)
         mov eax, [ebx]          ;   Cargo el segundo char* en eax, que deberia apuntar a un operador
         movzx eax, byte[eax]    ;   Cargo el primer char de la string operador
-        
+
         movzx ecx, byte[divs]
         cmp eax, ecx
         je .validated
@@ -79,5 +80,6 @@ main:
         add esp, 4*2
 
     .exit:
-        mov eax, 1
-        int 0x80
+        popad
+        leave 
+        ret
